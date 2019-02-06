e9c483231f714080bb99928e2bff318d2cd15c42
Andrzej Ruga <andrzej@ruga.pl>
2019-02-06 12:21:18 +0100

    User authentication status updating in the app

--- weight-tracker/src/components/records/LatestRecords.js
+++ weight-tracker/src/components/records/LatestRecords.js
@@ -8,8 +8,8 @@ const LatestRecords = ({records}) => {
             <h4 className="latest__title">Last 7 days results</h4>
             { records && records.map(record => {
                 return (
-                    <Link to={'/record/' + record.id}>
-                        <SingleRecord record={record} key={record.id} />
+                    <Link to={'/record/' + record.id} key={record.id}>
+                        <SingleRecord record={record} />
                     </Link>
                 )
             })}
--- weight-tracker/src/store/reducers/rootReducer.js
+++ weight-tracker/src/store/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import authReducer from './authReducer';
 import recordReducer from './recordReducer';
 import { combineReducers } from 'redux';
 import { firestoreReducer } from 'redux-firestore'; // premade reducer made for syncing firestore data with our state in the background
+import { firebaseReducer } from 'react-redux-firebase';
 
 const rootReducer = combineReducers({
     auth: authReducer,
     record: recordReducer,
-    firestore: firestoreReducer // responsible for syncing our data
+    firestore: firestoreReducer, // responsible for syncing our data
+    firebase: firebaseReducer
 });
 
 export default rootReducer;
