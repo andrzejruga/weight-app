a6d4ce0e16f8be29f7d8531cb978553805520cd0
Andrzej Ruga <andrzej@ruga.pl>
2019-02-06 13:28:36 +0100

    Logging in via the app done

--- weight-tracker/src/components/auth/SignIn.js
+++ weight-tracker/src/components/auth/SignIn.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { signIn } from '../../store/actions/authActions';
 
 class SignIn extends Component {
     state = {
@@ -12,9 +14,10 @@ class SignIn extends Component {
     }
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state);
+        this.props.signIn(this.state);
     }
     render() {
+        const { authError } = this.props;
         return (
             <div className="container container--main">
                 <form onSubmit={this.handleSubmit} className="form">
@@ -30,11 +33,25 @@ class SignIn extends Component {
                     <div className="form__input-field">
                         <button className="btn btn--login">Login</button>
                     </div>
+                    <div className="form__error-field">
+                        { authError ? <p>{authError}</p> : null }
+                    </div>
                 </form>
-
             </div>
         )
     }
 }
 
-export default SignIn;
+const mapStateToProps = (state) => {
+    return {
+        authError: state.auth.authError
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        signIn: (creds) => dispatch(signIn(creds))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
