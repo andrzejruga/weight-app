a96d47c09e592d0abb093948477361c26e5e4ecc
Andrzej Ruga <andrzej@ruga.pl>
2019-02-06 16:37:58 +0100

    Logging out via the app done

--- weight-tracker/src/components/layout/SignedInLinks.js
+++ weight-tracker/src/components/layout/SignedInLinks.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { signOut } from '../../store/actions/authActions';
 
-const SignedInLinks = () => {
+
+const SignedInLinks = (props) => {
     return (
         <ul className="nav__list">
             <li className="nav__item"><NavLink className="nav__link" to='/history'>History</NavLink></li>
-            <li className="nav__item"><NavLink className="nav__link" to='/'>Log Out</NavLink></li>
+            <li className="nav__item"><a href='/signin' onClick={props.signOut} className="nav__link">Log Out</a></li>
             <li className="nav__item"><NavLink className="nav__link" to='/'>AR</NavLink></li>
         </ul>
     )
 }
 
-export default SignedInLinks;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        signOut: () => dispatch(signOut())
+    }
+}
+
+export default connect(null, mapDispatchToProps)(SignedInLinks);
