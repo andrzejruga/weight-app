75d59339e937064d6d85a8ca11698359cb611bb5
Andrzej Ruga <andrzej@ruga.pl>
2019-02-07 13:33:30 +0100

    Updated History component so it syncs data with firestore

--- weight-tracker/src/components/records/History.js
+++ weight-tracker/src/components/records/History.js
@@ -1,44 +1,37 @@
 import React from 'react';
 import SingleRecord from './SingleRecord';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
+import { Link } from 'react-router-dom';
 
-const History = () => {
+const History = ({records}) => {
     return (
         <div className="history">
             <div className="history__container container container--main">
                 <h4 className="history__title">Last 30 days results</h4>
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
-                <SingleRecord />
+                { records && records.map(record => {
+                    return (
+                        <Link to={'/record/' + record.id} key={record.id}>
+                            <SingleRecord record={record} />
+                        </Link>
+                    )
+                })}
             </div>
         </div>
     )
 }
 
-export default History;
+const mapStateToProps = (state) => {
+    console.log(state);
+    return {
+        records: state.firestore.ordered.records
+    }
+}
+
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        { collection: 'records' } // this component will update our state everytime there's a change in our db
+    ])
+)(History);
--- weight-tracker/src/index.js
+++ weight-tracker/src/index.js
@@ -15,13 +15,18 @@ const store = createStore(rootReducer,
     compose(
         applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
         reduxFirestore(fbConfig),
-        reactReduxFirebase(fbConfig)
+        reactReduxFirebase(fbConfig, {attachAuthIsReady: true})
     )
 );
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+// thanks to AuthIsReady the app doesn't show sign in links in navbar for a few seconds
+// untill it realizes we are logged in and it starts showing sign out links
+// now we wait with rendering the DOM until firebase checks auth status
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+store.firebaseAuthIsReady.then(() => {
+    ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+    // Learn more about service workers: http://bit.ly/CRA-PWA
+    serviceWorker.unregister();
+})
