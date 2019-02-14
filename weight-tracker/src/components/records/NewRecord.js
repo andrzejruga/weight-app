0bbecd05fb110abefde8d1b10304b36e1de7b2e4
Andrzej Ruga <andrzej@ruga.pl>
2019-02-14 12:43:07 +0100

    Some more styling and default values for inputs

--- weight-tracker/src/components/records/NewRecord.js
+++ weight-tracker/src/components/records/NewRecord.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { addRecord } from '../../store/actions/recordActions';
+import moment from 'moment';
 
 class NewRecord extends Component {
     state = {
         weightOne: '',
         weightTwo: '',
-        date: ''
+        date: moment().format('YYYY-MM-D')
     }
     handleChange = (e) => {
         this.setState({
@@ -21,22 +22,24 @@ class NewRecord extends Component {
         this.props.addRecord(this.state);
     }
     render() {
+        // let today = moment().format('YYYY-MM-D');
+        // console.log(today);
         return (
             <div className="form">
                 <form onSubmit={this.handleSubmit} className="form__main">
                     <h4 className="form__title">Enter your weight</h4>
                     <div className="form__input-field">
                         <label htmlFor="weightOne" className="form__input-label">Monka</label>
-                        <input type="number" step="0.1" placeholder="00.0" id="weightOne" onChange={this.handleChange} className="form__input form__input--number"/>
+                        <input type="number" step="0.1" placeholder="00.0" min="2" max="200" id="weightOne" onChange={this.handleChange} className="form__input form__input--number"/>
                         <span>kg</span>
                     </div>
                     <div className="form__input-field">
                         <label htmlFor="date" className="form__input-label">Date</label>
-                        <input type="date" id="date" onChange={this.handleChange} className="form__input form__input--date" required/>
+                        <input type="date" id="date" defaultValue={moment().format('YYYY-MM-D')} onChange={this.handleChange} className="form__input form__input--date" required/>
                     </div>
                     <div className="form__input-field">
                         <label htmlFor="weightTwo" className="form__input-label">Andy</label>
-                        <input type="number" step="0.1" placeholder="00.0" id="weightTwo" onChange={this.handleChange} className="form__input form__input--number"/>
+                        <input type="number" step="0.1" placeholder="00.0" min="2" max="200" id="weightTwo" onChange={this.handleChange} className="form__input form__input--number"/>
                         <span>kg</span>
                     </div>
                     <div className="form__input-field">
