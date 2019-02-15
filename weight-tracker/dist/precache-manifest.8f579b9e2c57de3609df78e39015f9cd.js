ce311c0a5d0e72c2a932b8e8f1f9efd7dfafa583
Andrzej Ruga <andrzej@ruga.pl>
2019-02-15 17:05:55 +0100

    Deployed the app

---
+++ weight-tracker/dist/precache-manifest.8f579b9e2c57de3609df78e39015f9cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2535a952391309c2dadf",
+    "url": "/static/js/main.2535a952.chunk.js"
+  },
+  {
+    "revision": "63a51e12a4d7c9c9677c",
+    "url": "/static/js/1.63a51e12.chunk.js"
+  },
+  {
+    "revision": "2535a952391309c2dadf",
+    "url": "/static/css/main.a2b561c5.chunk.css"
+  },
+  {
+    "revision": "5528656a73ef01d7183f690a671a60a7",
+    "url": "/index.html"
+  }
+];
